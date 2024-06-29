dfd5b7be671f4f9c57478942bb508b177696b435
Raj Majunder <rajm97985@gmail.com>
2024-06-29 06:40:40 +0530

    enhance log out button

--- Frontend/src/components/Appbar.tsx
+++ Frontend/src/components/Appbar.tsx
@@ -1,9 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { ProfileLogo } from './ProfileLogo';
 import { useUserId } from "../hooks";
 
 export const Appbar = () => {
     const userId = useUserId();
+  const navigate = useNavigate()
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+       navigate("/signin")
+    };
 
     return (
         <div className="border-b bg-white shadow-md flex justify-between px-10 py-4">
@@ -11,9 +17,9 @@ export const Appbar = () => {
                 Dashboard
             </Link>
             <div className="flex items-center space-x-4">
-                <Link to="/signin" className="text-black px-4 py-2 hover:text-red-600">
+                <button onClick={handleLogout} className="text-black px-4 py-2 hover:text-red-600">
                     Log Out
-                </Link>
+                </button>
                 <Link to="/add">
                     <button className="bg-gray-200 text-black px-4 py-2 rounded hover:bg-gray-300 hover:text-black transition duration-200 ease-in-out">
                         Add Transaction
