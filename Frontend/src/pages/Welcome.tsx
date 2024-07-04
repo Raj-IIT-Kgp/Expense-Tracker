762980981ee73adca7fae5d69acb3e136edc7678
Raj Majunder <rajm97985@gmail.com>
2024-07-04 06:56:03 +0530

    center div

--- Frontend/src/pages/Welcome.tsx
+++ Frontend/src/pages/Welcome.tsx
@@ -5,7 +5,7 @@ const WelcomePage = () => {
             <header className="text-center">
                 <h1 className="text-6xl font-bold mb-4">Welcome to Expense Tracker</h1>
                 <p className="text-2xl mb-8">Track your expenses effortlessly and efficiently</p>
-                <div className="flex space-x-4">
+                <div className="flex space-x-4  col justify-center">
                     <Link to="/signup">
                         <button className="bg-white text-blue-500 font-semibold py-2 px-4 rounded shadow-lg transition-transform transform hover:scale-105">
                             Get Started
